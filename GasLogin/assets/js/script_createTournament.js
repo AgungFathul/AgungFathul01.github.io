459ea35b448b31123bb0fb601854b7416ef91e51
Raka Indra <128489066+Rahmaone@users.noreply.github.com>
2024-04-07 21:04:50 +0700

    revisi script_createTournament.js FIX
    tambah toggle on/off pada platformbutton dan lokasibutton, tambah script modifikasi tampilan form lokasi ketika salah satu lokasibutton diklik

--- GasLogin/assets/js/script_createTournament.js
+++ GasLogin/assets/js/script_createTournament.js
@@ -1,3 +1,4 @@
+//script untuk menu gamelist
 document.addEventListener("DOMContentLoaded", function() {
     var pcbutton = document.getElementById("pc");
     var mobilebutton = document.getElementById("mobile");
@@ -50,5 +51,90 @@ document.addEventListener("DOMContentLoaded", function() {
             "./assets/images/sports.jpg",
             "./assets/images/smash.jpg"
         ]);
+    });
+});
+
+// script untuk tampilan active platform button
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.querySelectorAll(".platformbutt");
+
+    buttons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            buttons.forEach(function(platformbutton) {
+                platformbutton.classList.remove("active");
+            });
+
+            this.classList.add("active");
+        });
+    });
+});
+
+// script untuk menu lokasi turnamen
+ const offlineBtn = document.getElementById("offlinebtn");
+ const onlineBtn = document.getElementById("onlinebtn");
+ const hybridBtn = document.getElementById("hybridbtn");
+ const namaTempatInputs = document.getElementsByClassName("namatempat");
+ const lokasi1 = document.querySelector(".lokasi-line1");
+ const lokasi2 = document.querySelector(".lokasi-line2");
+ const detailAlamatInput = document.querySelector(".detailalamat");
+ const detailAlamatHeader = document.querySelector(".detailalamat h6");
+ const keteranganInput = document.getElementById("keterangan");
+
+ function showInputBasedOnButton(buttonId) {
+    lokasi1.style.display = "none";
+    lokasi2.style.display = "none";
+    for (let input of namaTempatInputs) {
+      input.style.display = "none";
+    }
+    detailAlamatInput.style.display = "none";
+    keteranganInput.style.display = "none";
+
+    if (buttonId === "offlinebtn") {
+      lokasi1.style.display = "flex";
+      lokasi2.style.display = "flex";
+      for (let input of namaTempatInputs) {
+        input.style.display = "block";
+      }
+      detailAlamatInput.style.display = "block";
+      detailAlamatHeader.textContent = "Detail Alamat";
+      keteranganInput.style.display = "block";
+      keteranganInput.setAttribute("placeholder", "Detail alamat");
+    } else if (buttonId === "onlinebtn") {
+      detailAlamatInput.style.display = "block";
+      detailAlamatHeader.textContent = "Silahkan lanjut ke langkah berikutnya";
+      keteranganInput.style.display = "none";
+    } else if (buttonId === "hybridbtn") {
+      lokasi1.style.display = "flex";
+      lokasi2.style.display = "flex";
+      for (let input of namaTempatInputs) {
+        input.style.display = "block";
+      }
+      detailAlamatInput.style.display = "block";
+      detailAlamatHeader.textContent = "Keterangan Hybrid";
+      keteranganInput.style.display = "block";
+      keteranganInput.setAttribute("placeholder", "Keterangan untuk turnamen hybrid");
+    }
+ }
+
+ offlineBtn.addEventListener("click", () => showInputBasedOnButton("offlinebtn"));
+ onlineBtn.addEventListener("click", () => showInputBasedOnButton("onlinebtn"));
+ hybridBtn.addEventListener("click", () => showInputBasedOnButton("hybridbtn"));
+
+ window.addEventListener("load", function() {
+    showInputBasedOnButton("offlinebtn");
+});
+
+// script untuk lokasi button
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.querySelectorAll(".lokasi-buttons");
+
+    buttons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            buttons.forEach(function(lokasibuttons) {
+                lokasibuttons.classList.remove("active");
+            });
+
+            this.classList.add("active");
+        });
     });
 });
